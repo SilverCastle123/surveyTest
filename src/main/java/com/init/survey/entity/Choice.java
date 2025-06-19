403d7adeb9f558920b66a95bbcec5de1bcea0868
SilverCastle123 <chis7890@naver.com>
2025-06-19 16:09:40 +0900

    엔티티 추가

---
+++ src/main/java/com/init/survey/entity/Choice.java
@@ -0,0 +1,27 @@
+package com.init.survey.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SVY_CHOICE")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Choice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "choice_seq_gen")
+    @SequenceGenerator(name = "choice_seq_gen", sequenceName = "CHOICE_SEQ", allocationSize = 1)
+    private Long id;
+
+    @Column(length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
